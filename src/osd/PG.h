2d0e00f85b518c3f2da606cbd0c83f58a251efcb
Samuel Just <sjust@redhat.com>
2016-05-31 15:28:24 -0700

    PG.h: remove some unnecessary virtual declarations
    PG users do not need these public methods.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>
    Signed-off-by: Kefu Chai <kchai@redhat.com>
    Signed-off-by: Myoungwon Oh <omwmw@sk.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -2500,12 +2500,6 @@ public:
     ThreadPool::TPHandle &handle
   ) = 0;
 
-  virtual void do_op(OpRequestRef& op) = 0;
-  virtual void do_scan(
-    OpRequestRef op,
-    ThreadPool::TPHandle &handle
-  ) = 0;
-  virtual void do_backfill(OpRequestRef op) = 0;
   virtual void snap_trimmer(epoch_t epoch_queued) = 0;
 
   virtual int do_command(
